a921d60a62c57b1da717b083533699937c0c3598
Gokul <gokulgklas007@gmail.com>
2024-12-29 02:51:21 +0530

    jsfunc

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,33 @@
-export const cart = [];
+export const cart = [
+  {
+    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+  },
+  {
+    productId : '3fdfe8d6-9a15-4979-b459-585b0d0545b9',
+    quantity : 2
+  }
+];
+
+export function addToCart(productId)
+  {
+    let matchingItem;
+      cart.forEach((cartItem) => 
+      {
+        if(productId === cartItem.productId)
+        {
+          matchingItem = cartItem;
+        }
+      });
+        if(matchingItem)
+        {
+          matchingItem.quantity +=1;
+        }
+        else
+        {
+          cart.push({
+            productId: productId,
+            quantity : 1
+          });
+        }
+  }
